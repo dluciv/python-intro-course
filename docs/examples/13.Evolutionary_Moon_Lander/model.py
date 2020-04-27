d568f04d975cf6cbc8dd06f2274af9c451217321
Dmitry V. Luciv <dluciv@dluciv.name>
2020-04-27 21:50:30 +0300

    Two Captains

--- docs/examples/13.Evolutionary_Moon_Lander/model.py
+++ docs/examples/13.Evolutionary_Moon_Lander/model.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 from enum import Enum, IntFlag
+from abc import ABC, abstractmethod
 
 import numpy as np
-vec = np.array
+from numpy import array as vec
+from numpy.linalg import norm
 import csv
 from scipy import interpolate
 
@@ -22,6 +24,7 @@ class Surface:
     def get_height(self, x):
         return self._height(x)
 
+
 class Spaceship:
     class Thrust(IntFlag):
         NOPE = 0
@@ -30,41 +33,86 @@ class Spaceship:
         UP = 4
 
     _moon_g = 1.6
-    _engine_thrust = 1.0
-    _engine_mass_per_sec = 1.0
+    _engine_thrust = 3000.0
+    _engine_mass_per_sec = 3.0
 
     def __init__(self, mass: float, velocity: vec, position: vec):
-        self.mass = mass
-        self.velocity = velocity
-        self.position = position
-        self.thrust = Spaceship.Thrust.NOPE
+        self.maxlandingvelocity: float = 1.0
+        self.navigates = True
+        self.ok: bool = True
+        self.velocity: vec = velocity
+        self.position: vec = position
+        self.thrust: Spaceship.Thrust = Spaceship.Thrust.NOPE
+
+        self.dry_mass: float = mass / 2.0
+        self.fuel_mass: float = mass / 2.0
+
+    @property
+    def mass(self):
+        return self.dry_mass + self.fuel_mass
 
     def advance(self, delta_t: float):
         self.position += self.velocity * delta_t
-        self.velocity += [0, -Spaceship._moon_g * delta_t]
+        self.velocity += [0.0, -Spaceship._moon_g * delta_t]
 
-        thrust_vec = vec([0, 0])
-        dm = 0
-        if Spaceship.Thrust.RIGHT in self.thrust:
-            thrust_vec += [1, 0]
-            dm += 1
-        if Spaceship.Thrust.LEFT in self.thrust:
-            thrust_vec += [-1, 0]
-            dm += 1
-        if Spaceship.Thrust.UP in self.thrust:
-            thrust_vec += [0, 1]
-            dm += 1
+        thrust_vec = vec([0.0, 0.0])
+        dm = 0.0
 
-        self.velocity += thrust_vec * delta_t * engine_thrust / self.mass
-        self.mass -= Spaceship._engine_mass_per_sec * dm * delta_t
+        if self.fuel_mass > 0.0:
+            if Spaceship.Thrust.RIGHT in self.thrust:
+                thrust_vec += [0.25, 0.0]
+                dm += 0.25
+            if Spaceship.Thrust.LEFT in self.thrust:
+                thrust_vec += [-0.25, 0.0]
+                dm += 0.25
+            if Spaceship.Thrust.UP in self.thrust:
+                thrust_vec += [0.0, 1.0]
+                dm += 1.0
+
+        self.velocity += thrust_vec * delta_t * Spaceship._engine_thrust / self.mass
+        self.fuel_mass -= Spaceship._engine_mass_per_sec * dm * delta_t
+    
+    def land(self):
+        self.velocity = vec([0.0, 0.0])
+        self.navigates = False
+
+    def crash(self):
+        self.land()
+        self.ok = False
+
+class Captain(ABC):
+    def __init__(self, ):
+        pass
+
+    @abstractmethod
+    def control(self, surface: Surface, spaceship: Spaceship, time: float):
+        pass
+
 
 class Model:
-    def __init__(self, sf: Surface, ss: Spaceship):
-        self.surface = sf
-        self.spaceship = ss
+    def __init__(self, sf: Surface, ss: Spaceship, cap: Captain, delta_t = 0.01):
+        self.delta_t: float = delta_t
+        self.surface: Surface = sf
+        self.spaceship: Spaceship = ss
+        self.cap = cap
+        self.time = 0.0
 
-    def step(self):
-        pass
+    def step(self) -> bool:
+        self.cap.control(self.surface, self.spaceship, self.time)
+        self.spaceship.advance(self.delta_t)
+        self.time += self.delta_t
+        [sx, sy] = self.spaceship.position
+        sv = norm(self.spaceship.velocity)
+        if self.surface.get_height(sx) >= sy:
+            self.spaceship.navigates = False
+            if sv <= self.spaceship.maxlandingvelocity:
+                print("Landing ok!")
+                self.spaceship.land()
+            else:
+                print("Crash...")
+                self.spaceship.crash()
+                
+        return self.spaceship.navigates
 
 if __name__ == '__main__':
     raise NotImplementedError("This file is not supposed to be launched as a program")
--- docs/examples/13.Evolutionary_Moon_Lander/try_something.py
+++ docs/examples/13.Evolutionary_Moon_Lander/try_something.py
@@ -1,12 +1,61 @@
 #!/usr/bin/env python3
+from __future__ import annotations
+from typing import List
 
 import numpy as np
+from numpy import array as vec
+import matplotlib.pyplot as plt
+
 import model
+
+
+class BraveCaptain(model.Captain):
+    def control(self, surface: model.Surface, spaceship: model.Spaceship, time: float):
+        if 5.0 < time < 20.0:
+            spaceship.thrust = model.Spaceship.Thrust.UP
+        elif 25.0 < time < 40.0:
+            spaceship.thrust = model.Spaceship.Thrust.UP | model.Spaceship.Thrust.LEFT
+        else:
+            spaceship.thrust = model.Spaceship.Thrust.NOPE
+
+
+class CarefulCaptain(model.Captain):
+    def __init__(self):
+        self.count = 0
+
+    def control(self, surface: model.Surface, spaceship: model.Spaceship, time: float):
+        left = spaceship.velocity[0] > spaceship.maxlandingvelocity / 2.0
+        up =   spaceship.velocity[1] < -spaceship.maxlandingvelocity / 2.0
+
+        spaceship.thrust = \
+            (model.Spaceship.Thrust.LEFT if left else model.Spaceship.Thrust.NOPE) | \
+            (model.Spaceship.Thrust.UP if up else model.Spaceship.Thrust.NOPE)
+
+        if self.count % 1000 == 0:
+            print(time, spaceship.thrust, spaceship.mass)
+        self.count += 1
+
+
+def trackship(m: model.Model) -> List[vec]:
+    poss = []
+    while m.step():
+        poss.append(m.spaceship.position.copy())
+    return poss
 
 if __name__ == '__main__':
     sur = model.Surface("surface_heights.csv", 2000.0)
 
-    import matplotlib.pyplot as plt
+    m = model.Model(
+        sur,
+        model.Spaceship(1000.0, vec([25.0, 0.0]), vec([0.0, 200.0])),
+        CarefulCaptain()
+    )
+
     xs = np.arange(0.0, sur.get_width())
     plt.plot(xs, [sur.get_height(x) for x in xs])
+
+    poss = trackship(m)
+    tx, ty = tuple(zip(*poss))
+    plt.plot(tx, ty)
+
     plt.show()
