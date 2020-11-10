36e11bdcbd520a7df0b694e258478ee8484eed8f
Dmitry V. Luciv <dluciv@dluciv.name>
2020-11-10 17:01:30 +0300

    Sort of spedup

--- docs/examples/01_5.Turtle/two_turtles.py
+++ docs/examples/01_5.Turtle/two_turtles.py
@@ -3,14 +3,15 @@ import time
 import math
 import numpy as np
 
-MODEL_DT = 0.05
+MODEL_DT = 0.01
 MODEL_G = 9.81
 MODEL_T = 15
 
 sc = tl.Screen()    # показ экрана
 tl.hideturtle()     # спрятать черепашку по умолчанию
 tl.tracer(0,0)      # отключение автоматического обновления экрана
-tl.speed(0)
+tl.speed(0)         # максимальная скорость прорисовки
+tl.delay(0)         # отключение 10-мс задержки перед прорисовкой
 
 class Body:
     def __init__(self, x, y, vx, vy, color = 'black'):
