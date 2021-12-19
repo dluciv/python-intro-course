0857f0a5d658c41f58be601b8bad6cc16b35b037
Dmitry V. Luciv <dluciv@dluciv.name>
2021-12-19 20:10:02 +0300

    Upd prime check

--- docs/examples/01.quality/prime_check_with_tests.py
+++ docs/examples/01.quality/prime_check_with_tests.py
@@ -5,7 +5,6 @@
 """
 
 import unittest
-from sympy.functions.combinatorial.numbers import carmichael
 from sympy.ntheory import primefactors
 
 def is_prime(num):
@@ -51,12 +50,14 @@ class PrimeCheckTest(unittest.TestCase):
 
     def test_carmichael(self):
         """Проверка на числах Кармайкла"""
+        # можно было sympy.functions.combinatorial.numbers.carmichael,
+        # но очень не быстро, лучше взять готовые https://oeis.org/A002997
         carmichaels = [
             561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341,
             41041, 46657, 52633, 62745, 63973, 75361, 101101, 115921, 126217,
             162401, 172081, 188461, 252601, 278545, 294409, 314821, 334153,
             340561, 399001, 410041, 449065, 488881, 512461
-        ]  # https://oeis.org/A002997, sympy их ищет очень долго
+        ]
 
         for _ in range(10000):
             for c in carmichaels:
